c7ec8c83a76539363cecae269f0a3a22d2040fde
Javier Larroca <javier-larroca@hotmail.com>
2025-08-21 22:45:02 -0300

    Practica realizada hasta unidad 2

---
+++ importar.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+// import "math/rand"
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Scan()
+	strings.Map()
+}
---
+++ primerapp.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	fmt.Print("Hola mundo")
+// }
+
+func main() {
+	var nombre string = "Javier"
+	fmt.Print("Hola " + nombre)
+}
